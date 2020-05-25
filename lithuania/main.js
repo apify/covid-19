d0297586f457ec3e64f3102724868405fce30e5d
Zuzana Pelechová <zpelechova@gmail.com>
2020-05-25 11:15:57 +0200

    changed Lithuania

--- lithuania/main.js
+++ lithuania/main.js
@@ -1,62 +1,123 @@
+
 const Apify = require('apify');
-const moment = require('moment-timezone');
-const _ = require('lodash');
-
-const { log } = Apify.utils;
-log.setLevel(log.LEVELS.WARNING);
-
-const LATEST ='LATEST';
+const httpRequest = require('@apify/http-request')
+const cheerio = require('cheerio');
+const sourceUrl = 'http://sam.lrv.lt/lt/naujienos/koronavirusas';
+const LATEST = 'LATEST';
 
 Apify.main(async () => {
-    const sourceUrl = 'http://sam.lrv.lt/lt/naujienos/koronavirusas';
+
     const kvStore = await Apify.openKeyValueStore("COVID-19-LITHUANIA");
     const dataset = await Apify.openDataset("COVID-19-LITHUANIA-HISTORY");
 
-    const requestList = new Apify.RequestList({
-        sources: [
-            { url: sourceUrl },
-        ],
-    });
-    await requestList.initialize();
+    console.log('Getting data...');
+    const { body } = await httpRequest({ url: sourceUrl });
+    const $ = cheerio.load(body);
+    const infected = $('#module_Structure > div > div.main_content.clearfix > div:nth-child(3) > div.text > ul:nth-child(6) > li:nth-child(1) > span > span > span > b').text()
+    const deceased = $('#module_Structure > div > div.main_content.clearfix > div:nth-child(3) > div.text > ul:nth-child(6) > li:nth-child(4) > span > span > span > b').text();
+    const recovered = $("#module_Structure > div > div.main_content.clearfix > div:nth-child(3) > div.text > ul:nth-child(6) > li:nth-child(6) > span > span > span > b").text();
+    const newInfected = $("#module_Structure > div.wrapper > div.main_content.clearfix > div:nth-child(3) > div.text > ul:nth-child(6) > li:nth-child(3) > span > span > span > b").text();
+    const isolated = $("#module_Structure > div.wrapper > div.main_content.clearfix > div:nth-child(3) > div.text > ul:nth-child(6) > li:nth-child(7) > span > span > span > b").text();
+    const connectedDeaths = $('#module_Structure > div.wrapper > div.main_content.clearfix > div:nth-child(3) > div.text > ul:nth-child(6) > li:nth-child(5) > span > span > span > b').text();
+    
+    
+    const now = new Date();
 
-    const crawler = new Apify.CheerioCrawler({
-        requestList,
-        maxRequestRetries: 1,
-        handlePageTimeoutSecs: 60,
 
-        handlePageFunction: async ({ request, $ }) => {
-            log.info(`Processing ${request.url}...`);
+    const result = {
+        infected: infected,
+        recovered: recovered,
+        deceased: deceased,
+        newInfected: newInfected,
+        isolated: isolated,
+        connectedDeaths: connectedDeaths,
+        sourceUrl: 'http://sam.lrv.lt/lt/naujienos/koronavirusas',
+        lastUpdatedAtApify: new Date(Date.UTC(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(), now.getMinutes())).toISOString(),
+        readMe: 'https://apify.com/dtrungtin/covid-lt'
+    };
+    console.log(result)
 
-            const data = {
-                country: 'Lithuania',
-                sourceUrl,
-                lastUpdatedAtApify: moment().utc().second(0).millisecond(0).toISOString(),
-                readMe: "https://apify.com/dtrungtin/covid-lt",
-            };
-            const textUls = $('.text > ul');
-            const [confirmed] = $('.text > ul > li:nth-child(1)').text().match(/\d+/);
-            const [t, died] = $('.text > ul > li:nth-child(4)').text().match(/\d+/g);
-            const [tested] = textUls.eq(1).find(' > li:nth-child(2)').text().match(/\d+/);
-            data.confirmedCases = parseInt(confirmed);
-            data.testedCases = parseInt(tested);
-            data.numberOfDeaths = parseInt(died);
+    let latest = await kvStore.getValue(LATEST);
+    if (!latest) {
+        await kvStore.setValue('LATEST', result);
+        latest = result;
+    }
+    delete latest.lastUpdatedAtApify;
+    const actual = Object.assign({}, result);
+    delete actual.lastUpdatedAtApify;
 
-            // Compare and save to history
-            const latest = await kvStore.getValue(LATEST) || {};
-            if (!_.isEqual(_.omit(data, 'lastUpdatedAtApify'), _.omit(latest, 'lastUpdatedAtApify'))) {
-                await dataset.pushData(data);
-            }
+    if (JSON.stringify(latest) !== JSON.stringify(actual)) {
+        await dataset.pushData(result);
+    }
 
-            await kvStore.setValue(LATEST, data);
-            await Apify.pushData(data);
-        },
+    await kvStore.setValue('LATEST', result);
+    await Apify.pushData(result);
+}
+);
 
-        handleFailedRequestFunction: async ({ request }) => {
-            log.info(`Request ${request.url} failed twice.`);
-        },
-    });
 
-    await crawler.run();
 
-    log.info('Crawler finished.');
-});
+// old actor
+
+// const Apify = require('apify');
+// const moment = require('moment-timezone');
+// const _ = require('lodash');
+
+// const { log } = Apify.utils;
+// log.setLevel(log.LEVELS.WARNING);
+
+// const LATEST ='LATEST';
+
+// Apify.main(async () => {
+//     const sourceUrl = 'http://sam.lrv.lt/lt/naujienos/koronavirusas';
+//     const kvStore = await Apify.openKeyValueStore("COVID-19-LITHUANIA");
+//     const dataset = await Apify.openDataset("COVID-19-LITHUANIA-HISTORY");
+
+//     const requestList = new Apify.RequestList({
+//         sources: [
+//             { url: sourceUrl },
+//         ],
+//     });
+//     await requestList.initialize();
+
+//     const crawler = new Apify.CheerioCrawler({
+//         requestList,
+//         maxRequestRetries: 1,
+//         handlePageTimeoutSecs: 60,
+
+//         handlePageFunction: async ({ request, $ }) => {
+//             log.info(`Processing ${request.url}...`);
+
+//             const data = {
+//                 country: 'Lithuania',
+//                 sourceUrl,
+//                 lastUpdatedAtApify: moment().utc().second(0).millisecond(0).toISOString(),
+//                 readMe: "https://apify.com/dtrungtin/covid-lt",
+//             };
+//             const textUls = $('.text > ul');
+//             const [confirmed] = $('.text > ul > li:nth-child(1)').text().match(/\d+/);
+//             const [t, died] = $('.text > ul > li:nth-child(4)').text().match(/\d+/g);
+//             const [tested] = textUls.eq(1).find(' > li:nth-child(2)').text().match(/\d+/);
+//             data.confirmedCases = parseInt(confirmed);
+//             data.testedCases = parseInt(tested);
+//             data.numberOfDeaths = parseInt(died);
+
+//             // Compare and save to history
+//             const latest = await kvStore.getValue(LATEST) || {};
+//             if (!_.isEqual(_.omit(data, 'lastUpdatedAtApify'), _.omit(latest, 'lastUpdatedAtApify'))) {
+//                 await dataset.pushData(data);
+//             }
+
+//             await kvStore.setValue(LATEST, data);
+//             await Apify.pushData(data);
+//         },
+
+//         handleFailedRequestFunction: async ({ request }) => {
+//             log.info(`Request ${request.url} failed twice.`);
+//         },
+//     });
+
+//     await crawler.run();
+
+//     log.info('Crawler finished.');
+// });
