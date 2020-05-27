a729a2abfc567d8edf8452ccd5a9517d199a5d04
Zuzana Pelechová <zpelechova@gmail.com>
2020-05-28 01:24:29 +0200

    check actor update

--- covid-check/main.js
+++ covid-check/main.js
@@ -45,10 +45,10 @@ Apify.main(async () => {
         if (result[key].deviation && Math.abs(result[key].deviation) >= 5) highDeviation = true;
     }
     
-    // that's it, let's save the file
-    await Apify.setValue('result', result);
-    // await Apify.setValue('OUTPUT', result.filter(x => (x.deviation > 0 || x.deviation < 0 )));
-    await Apify.pushData('result');
+    // // that's it, let's save the file
+    // await Apify.setValue('result', result);
+    // // await Apify.setValue('OUTPUT', result.filter(x => (x.deviation > 0 || x.deviation < 0 )));
+    // await Apify.pushData('result');
 
     // if there's at least one country with deviation over 5%
     if (highDeviation) {
