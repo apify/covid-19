123a11c24fdeb3ecb4c1760fdcd49c6e81711183
laidihalim <abdelhalim@onidivo.com>
2020-06-03 12:49:46 +0100

    fix - Slovenia - wrong number

--- slovenia/main.js
+++ slovenia/main.js
@@ -1,90 +1,97 @@
+// main.js
 const Apify = require('apify');
-// const moment = require('moment-timezone');
-const _ = require('lodash');
-
+const XLSX = require('xlsx')
 const { log } = Apify.utils;
-// log.setLevel(log.LEVELS.WARNING);
 
-const LATEST = 'LATEST';
+const LATEST = "LATEST";
+const now = new Date();
+const sourceUrl = 'https://www.gov.si/en/topics/coronavirus-disease-covid-19/actual-data/';
 
 Apify.main(async () => {
-    const sourceUrl = 'https://www.gov.si/en/topics/coronavirus-disease-covid-19/';
+
+    log.info('Starting actor.');
+
     const kvStore = await Apify.openKeyValueStore("COVID-19-SLOVENIA");
     const dataset = await Apify.openDataset("COVID-19-SLOVENIA-HISTORY");
-
     const requestList = new Apify.RequestList({
         sources: [
-            { url: sourceUrl },
+            {
+                url: 'http://www.gov.si/assets/vlada/Koronavirus-podatki/en/EN_Covid-19-all-data.xlsx',
+            }
         ],
     });
+
     await requestList.initialize();
 
-    const crawler = new Apify.CheerioCrawler({
+    log.debug('Setting up crawler.');
+    const cheerioCrawler = new Apify.CheerioCrawler({
         requestList,
-        maxRequestRetries: 1,
-        handlePageTimeoutSecs: 60,
+        maxRequestRetries: 5,
+        requestTimeoutSecs: 90,
+        useApifyProxy: true,
+        additionalMimeTypes: ['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'text/plain'],
+        prepareRequestFunction: async ({ request }) => {
+            log.info(`Downloading xlsx file ...`)
+        },
+        handlePageFunction: async ({ body }) => {
 
-        handlePageFunction: async ({ request, $ }) => {
-            log.info(`Processing ${request.url}...`);
+            log.info(`File had downloaded.`);
 
-            log.info('Processing and saving data')
+            log.info(`Processing and saving data.`);
+
+            let workbook = XLSX.read(body, { type: "buffer" });
+
+            const { ModifiedDate } = workbook.Props;
+            const atSource = new Date(ModifiedDate)
+
+            const lastApdate = XLSX.utils.sheet_to_json(workbook.Sheets['Covid-19 podatki']).pop();
+
             const data = {
-                sourceUrl,
-                // lastUpdatedAtApify: moment().utc().second(0).millisecond(0).toISOString(),
-                readMe: "https://apify.com/dtrungtin/covid-si",
-            };
-
-            const columns = $('table tbody tr td');
-            if (columns.length > 0) {
-                const confirmedDateText = $(columns[0]).text();
-                const tested = parseInt($(columns[1]).text());
-                const positive = parseInt($(columns[3]).text());
-                const hospitalized = parseInt($(columns[3]).text());
-                const intensiveCare = parseInt($(columns[4]).text());
-                const discharged = parseInt($(columns[5]).text());
-                const died = parseInt($(columns[6]).text());
-
-                const infected = positive + hospitalized + intensiveCare;
-
-                // const matchUpadatedAt = confirmedDateText.match(/(\d+).\s+(\d+).\s+(\d+)/);
-
-                // if (matchUpadatedAt && matchUpadatedAt.length > 3) {
-                //     data.lastUpdatedAtSource = moment({
-                //         year: parseInt(matchUpadatedAt[3]),
-                //         month: parseInt(matchUpadatedAt[2]) - 1,
-                //         date: parseInt(matchUpadatedAt[1]),
-                //         hour: 0,
-                //         minute: 0,
-                //         second: 0,
-                //         millisecond: 0
-                //     }).toISOString();
-                // } else {
-                //     throw new Error('lastUpdatedAtSource not found');
-                // }
-
-                data.testedCases = tested;
-                data.infectedCases = positive;
-                data.numberOfDeath = died;
-                data.discharged = discharged;
+                testedCases: lastApdate['Tested (all)'],
+                infectedCases: lastApdate['Positive (all)'],
+                numberOfDeath: lastApdate['Deaths (all)'],
+                dailyTested: lastApdate['Tested (daily)'],
+                dailyInfected: lastApdate['Positive (daily)'],
+                dailyDeaths: lastApdate['Deaths (daily)'],
+                dailyDischarged: lastApdate.Discharged,
+                dailyHospitalized: lastApdate['All hospitalized on certain day'],
+                dailyIntensiveCare: lastApdate['All persons in intensive care on certain day'],
+                country: 'slovenia',
+                historyData: 'https://api.apify.com/v2/datasets/H6HKZRQr8I81bClnb/items?format=json&clean=1',
+                sourceUrl: sourceUrl,
+                lastUpdatedAtApify: new Date(Date.UTC(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(), now.getMinutes())).toISOString(),
+                lastUpdatedAtSource: new Date(Date.UTC(atSource.getFullYear(), atSource.getMonth(), atSource.getDate(), (atSource.getHours()), atSource.getMinutes())).toISOString(),
+                readMe: 'https://apify.com/dtrungtin/covid-si'
             }
 
-            // Compare and save to history
-            const latest = await kvStore.getValue(LATEST) || {};
-            if (!_.isEqual(_.omit(data, 'lastUpdatedAtApify'), _.omit(latest, 'lastUpdatedAtApify'))) {
+            // Push the data
+            let latest = await kvStore.getValue(LATEST);
+            if (!latest) {
+                await kvStore.setValue('LATEST', data);
+                latest = Object.assign({}, data);
+            }
+            delete latest.lastUpdatedAtApify;
+            const actual = Object.assign({}, data);
+            delete actual.lastUpdatedAtApify;
+
+            const { itemCount } = await dataset.getInfo();
+            if (JSON.stringify(latest) !== JSON.stringify(actual) || itemCount === 0) {
                 await dataset.pushData(data);
             }
 
-            await kvStore.setValue(LATEST, data);
+            await kvStore.setValue('LATEST', data);
             await Apify.pushData(data);
-            log.info('data saved')
-        },
 
+            log.info('Data had saved.');
+        },
         handleFailedRequestFunction: async ({ request }) => {
-            log.info(`Request ${request.url} failed twice.`);
+            console.log(`Request ${request.url} failed many times.`);
+            console.dir(request)
         },
     });
-
-    await crawler.run();
-
-    log.info('Crawler finished.');
+    // Run the crawler and wait for it to finish.
+    log.info('Starting the crawl.');
+    await cheerioCrawler.run();
+    log.info('Actor finished.');
 });
+
