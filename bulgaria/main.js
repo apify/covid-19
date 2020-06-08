c7665cf6f7397f28a349ea8ee8c8f22de2d5b8e6
Zuzana Pelechová <zpelechova@gmail.com>
2020-06-08 10:56:11 +0200

    fixed Bulgaria

--- bulgaria/main.js
+++ bulgaria/main.js
@@ -1,89 +1,54 @@
 const Apify = require('apify');
-
-const sourceUrl = 'https://www.mh.government.bg/bg/informaciya-za-grazhdani/potvrdeni-sluchai-na-koronavirus-na-teritoriyata-na-r-blgariya/';
+const httpRequest = require('@apify/http-request')
+const cheerio = require('cheerio');
+const sourceUrl = 'https://coronavirus.bg/bg/';
 const LATEST = 'LATEST';
-let check = false;
 
 Apify.main(async () => {
-
     const kvStore = await Apify.openKeyValueStore('COVID-19-BG');
     const dataset = await Apify.openDataset('COVID-19-BG-HISTORY');
-    const { email } = await Apify.getValue('INPUT');
-
-    console.log('Launching Puppeteer...');
-    const browser = await Apify.launchPuppeteer();
-
-    const page = await browser.newPage();
-    await Apify.utils.puppeteer.injectJQuery(page);
-
-    console.log('Going to the website...');
-    await page.goto(sourceUrl), { waitUntil: 'networkidle0', timeout: 60000 };
-
-    await page.waitForSelector('#top > div.container > div > div.col-xs-12.col-sm-12.col-md-8 > table > tbody > tr:nth-child(1) > td:nth-child(2) > p');
 
     console.log('Getting data...');
-    // page.evaluate(pageFunction[, ...args]), pageFunction <function|string> Function to be evaluated in the page context, returns: <Promise<Serializable>> Promise which resolves to the return value of pageFunction
-    const result = await page.evaluate(() => {
-        const now = new Date();
+    const { body } = await httpRequest({ url: sourceUrl });
+    const $ = cheerio.load(body);
+    const tested = $('body > main > div.container-fluid.main-content-top > div > div.row.statistics-container > div.col-lg-3.col-md-6 > p.statistics-value').text();
+    const infected = $('body > main > div.container-fluid.main-content-top > div > div.row.statistics-container > div:nth-child(2) > p.statistics-value.confirmed').text();
+    const activeCases = $('body > main > div.container-fluid.main-content-top > div > div.row.statistics-container > div:nth-child(2) > p.statistics-subvalue').text();
+    const hospitalised = $('body > main > div.container-fluid.main-content-top > div > div.row.statistics-container > div:nth-child(3) > p.statistics-value').text();
+    const ICU = $('body > main > div.container-fluid.main-content-top > div > div.row.statistics-container > div:nth-child(3) > p.statistics-subvalue').text();
+    const recovered = $('body > main > div.container-fluid.main-content-top > div > div.row.statistics-container > div:nth-child(4) > p.statistics-value.healed').text();
+    const deceased = $('body > main > div.container-fluid.main-content-top > div > div.row.statistics-container > div:nth-child(5) > p.statistics-value.deceased').text();
 
-        // eq() selector selects an element with a specific index number, text() method sets or returns the text content of the selected elements
-        const infected = $('#top > div.container > div > div.col-xs-12.col-sm-12.col-md-8 > table > tbody > tr:nth-child(1) > td:nth-child(2) > p').text();
-        const deceased = $('#top > div.container > div > div.col-xs-12.col-sm-12.col-md-8 > table > tbody > tr:nth-child(2) > td:nth-child(2)').text();
-        const recovered = $("#top > div.container > div > div.col-xs-12.col-sm-12.col-md-8 > table > tbody > tr:nth-child(3) > td:nth-child(2)").text();
+    const now = new Date();
 
-        const data = {
-            infected: infected,
-            deceased: deceased,
-            recovered: recovered,
-            sourceUrl: 'https://www.mh.government.bg/bg/informaciya-za-grazhdani/potvrdeni-sluchai-na-koronavirus-na-teritoriyata-na-r-blgariya/',
-            lastUpdatedAtApify: new Date(Date.UTC(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(), now.getMinutes())).toISOString(),
-            readMe: 'https://github.com/zpelechova/covid-bg/blob/master/README.md',
-        };
-        return data;
-
-    });
-
+    const result = {
+        tested,
+        infected,
+        activeCases,
+        hospitalised, 
+        ICU, 
+        recovered,
+        deceased,
+        sourceUrl: 'https://coronavirus.bg/bg/',
+        lastUpdatedAtApify: new Date(Date.UTC(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(), now.getMinutes())).toISOString(),
+        readMe: 'https://apify.com/zuzka/covid-bg'
+    };
     console.log(result)
 
-    if (!result.infected || !result.deceased || !result.recovered) {
-        check = true;
-    }
-    else {
-        let latest = await kvStore.getValue(LATEST);
-        if (!latest) {
-            await kvStore.setValue('LATEST', result);
-            latest = result;
-        }
-        delete latest.lastUpdatedAtApify;
-        const actual = Object.assign({}, result);
-        delete actual.lastUpdatedAtApify;
-
-        if (JSON.stringify(latest) !== JSON.stringify(actual)) {
-            await dataset.pushData(result);
-        }
-
+    let latest = await kvStore.getValue(LATEST);
+    if (!latest) {
         await kvStore.setValue('LATEST', result);
-        await Apify.pushData(result);
+        latest = result;
+    }
+    delete latest.lastUpdatedAtApify;
+    const actual = Object.assign({}, result);
+    delete actual.lastUpdatedAtApify;
+
+    if (JSON.stringify(latest) !== JSON.stringify(actual)) {
+        await dataset.pushData(result);
     }
 
-
-    console.log('Closing Puppeteer...');
-    await browser.close();
-    console.log('Done.');
-
-    // if there are no data for TotalInfected, send email, because that means something is wrong
-    const env = await Apify.getEnv();
-    if (check) {
-        await Apify.call(
-            'apify/send-mail',
-            {
-                to: email,
-                subject: `Covid-19 BG from ${env.startedAt} failed `,
-                html: `Hi, ${'<br/>'}
-                        <a href="https://my.apify.com/actors/${env.actorId}#/runs/${env.actorRunId}">this</a> 
-                        run had 0 TotalInfected, check it out.`,
-            },
-            { waitSecs: 0 },
-        );
-    };
-});
+    await kvStore.setValue('LATEST', result);
+    await Apify.pushData(result);
+}
+);
