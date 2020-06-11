3557c7be86be777022772f3ab86c3e787faec468
Zuzana Pelechová <zpelechova@gmail.com>
2020-06-11 10:25:21 +0200

    lithuania again

--- lithuania/main.js
+++ lithuania/main.js
@@ -17,9 +17,9 @@ Apify.main(async () => {
     const deceased = $('#module_Structure > div.wrapper > div.main_content.clearfix > div:nth-child(3) > div.text > ul > li:nth-child(4) > strong').text();
     const recovered = $("#module_Structure > div.wrapper > div.main_content.clearfix > div:nth-child(3) > div.text > ul > li:nth-child(6) > strong").text();
     const newInfected = $("#module_Structure > div.wrapper > div.main_content.clearfix > div:nth-child(3) > div.text > ul > li:nth-child(3) > strong").text();
-    const isolated = $("#module_Structure > div.wrapper > div.main_content.clearfix > div:nth-child(3) > div.text > ul > li:nth-child(3) > strong").text();
+    const isolated = $("#module_Structure > div.wrapper > div.main_content.clearfix > div:nth-child(3) > div.text > ul > li:nth-child(7) > strong").text();
     const connectedDeaths = $('#module_Structure > div.wrapper > div.main_content.clearfix > div:nth-child(3) > div.text > ul > li:nth-child(5) > strong').text();
-    const stillSick = $('#module_Structure > div.wrapper > div.main_content.clearfix > div:nth-child(3) > div.text > ul > li:nth-child(2) > strong').text();
+    const stillSick = $('#module_Structure > div.wrapper > div.main_content.clearfix > div:nth-child(3) > div.text > ul > li:nth-child(2) > b').text();
     
     const now = new Date();
 
