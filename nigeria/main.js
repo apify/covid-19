70215db4395317d2026f730210db0e52b573147d
Zuzana Pelechová <zpelechova@gmail.com>
2020-06-22 12:00:20 +0200

    Nigeria fixed

--- nigeria/main.js
+++ nigeria/main.js
@@ -2,13 +2,11 @@ const Apify = require('apify');
 
 const sourceUrl = 'https://covid19.ncdc.gov.ng/';
 const LATEST = 'LATEST';
-let check = false;
 
 Apify.main(async () => {
 
     const kvStore = await Apify.openKeyValueStore('COVID-19-NG');
     const dataset = await Apify.openDataset('COVID-19-NG-HISTORY');
-    const { email } = await Apify.getValue('INPUT');
 
     console.log('Launching Puppeteer...');
     const browser = await Apify.launchPuppeteer();
@@ -30,19 +28,20 @@ Apify.main(async () => {
         const deceased = $('body > div.pcoded-main-container > div > div.page-header > div:nth-child(2) > div:nth-child(4) > div > div > h2 > span').text();
 
         const regionsTableRows = Array.from(document.querySelectorAll("#custom1 > tbody > tr"));
+
         const regionData = [];
 
         for (const row of regionsTableRows) {
-            const strip = (a) => Number(a.trim())
+            const strip = (a) => Number(a.replace(",","").trim())
             const cells = Array.from(row.querySelectorAll("td")).map(td => td.textContent);
             regionData.push({ region: cells[0].trim(), labConfirmedCases: strip(cells[1]), onAdmissionCases: strip(cells[2]), discharged: strip(cells[3]), deaths: strip(cells[4]) });
         }
 
 
-        const toInt = (a) => Number(a.replace(',', ''))
+        const toInt = (a) => Number(a.replace(',', '').trim())
 
         const data = {
-            tested: toInt(tested),
+            tested: tested,
             infected: toInt(infected),
             recovered: toInt(recovered),
             deceased: toInt(deceased),
@@ -52,7 +51,7 @@ Apify.main(async () => {
             lastUpdatedAtApify: new Date(Date.UTC(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(), now.getMinutes())).toISOString(),
             lastUpdatedAtSource: 'N/A',
             readMe: 'https://github.com/zpelechova/covid-ng/blob/master/README.md',
-            activeCases: toInt(activeCases),
+            activeCases: toInt(activeCases.replace(",", "")),
             regions: regionData,
         };
         return data;
@@ -61,45 +60,23 @@ Apify.main(async () => {
 
     console.log(result)
 
-    if (!result.infected || !result.deceased || !result.recovered) {
-        check = true;
-    }
-    else {
-        let latest = await kvStore.getValue(LATEST);
-        if (!latest) {
-            await kvStore.setValue('LATEST', result);
-            latest = result;
-        }
-        delete latest.lastUpdatedAtApify;
-        const actual = Object.assign({}, result);
-        delete actual.lastUpdatedAtApify;
-
-        if (JSON.stringify(latest) !== JSON.stringify(actual)) {
-            await dataset.pushData(result);
-        }
-
+    let latest = await kvStore.getValue(LATEST);
+    if (!latest) {
         await kvStore.setValue('LATEST', result);
-        await Apify.pushData(result);
+        latest = result;
+    }
+    delete latest.lastUpdatedAtApify;
+    const actual = Object.assign({}, result);
+    delete actual.lastUpdatedAtApify;
+
+    if (JSON.stringify(latest) !== JSON.stringify(actual)) {
+        await dataset.pushData(result);
     }
 
+    await kvStore.setValue('LATEST', result);
+    await Apify.pushData(result);
 
     console.log('Closing Puppeteer...');
     await browser.close();
     console.log('Done.');
-
-    // if there are no data for TotalInfected, send email, because that means something is wrong
-    const env = await Apify.getEnv();
-    if (check) {
-        await Apify.call(
-            'apify/send-mail',
-            {
-                to: email,
-                subject: `Covid-19 NG from ${env.startedAt} failed `,
-                html: `Hi, ${'<br/>'}
-                        <a href="https://my.apify.com/actors/${env.actorId}#/runs/${env.actorRunId}">this</a> 
-                        run had 0 TotalInfected, check it out.`,
-            },
-            { waitSecs: 0 },
-        );
-    };
 });
