0c703fef26e6dc97a02b8972e919ff85b1062051
Zuzana Pelechová <zpelechova@gmail.com>
2020-07-20 08:00:46 +0200

    honduras first commit

---
+++ honduras/src/routes.js
@@ -0,0 +1,15 @@
+const Apify = require('apify');
+
+const { utils: { log } } = Apify;
+
+exports.handleStart = async ({ request, $ }) => {
+    // Handle Start URLs
+};
+
+exports.handleList = async ({ request, $ }) => {
+    // Handle pagination
+};
+
+exports.handleDetail = async ({ request, $ }) => {
+    // Handle details
+};
