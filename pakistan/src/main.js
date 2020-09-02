d5908dd949bd4130a1e9fb58751d1e40c3f40a35
Zuzana Pelechová <zpelechova@gmail.com>
2020-09-02 11:48:05 +0200

    Changed Pakistan selectors

--- pakistan/src/main.js
+++ pakistan/src/main.js
@@ -25,35 +25,40 @@ Apify.main(async () => {
 
     log.info('Processing and saving data...');
 
-    const infected = $(".numanimate").text();
-    const tested = $('h6:contains("Total Tests")').next().text();
-    const recovered = $('div.revovered.covid-number-box').text();
-    const deceased = $('div.deaths.covid-number-box').text();
-    const lastLocal = $("#date")
-        .text()
-        .trim()
-        .split(': ')[1]
-        .split('- ')
-        .map((input, index) =>
-            index ?
-                input.replace(/[a-z]+/g, ` ${input.match(/[a-z]+/)}`) :
-                input)
-        .join('')
-        .replace(/\s+/g, ' ');
-    const dateLocal = new Date(`${lastLocal} GMT+5`);
+    const infected = $('.diagnosed-icon').next().next().text().replace(',','');
+    const tested = $('.active-icon').next().next().text().replace(',','').replace(',','');
+    const recovered = $('.recovered-icon').next().next().text().replace(',','');
+    const deceased = $('.deaths-icon').next().next().text().replace(',','');
+    const critical = $('.tests-icon').next().next().text().replace(',','');
+    
+    // const lastLocal = $("#last-update")
+    //     .text()
+    //     .trim()
+    //     .split(':')[1]
+    //     .split('- ')
+    //     .map((input, index) =>
+    //         index ?
+    //             input.replace(/[a-z]+/g, ` ${input.match(/[a-z]+/)}`) :
+    //             input)
+    //     .join('')
+    //     .replace(/\s+/g, ' ');
+    // const dateLocal = new Date(`${lastLocal} GMT+5`);
     // const lastUpdatedAtSource = new Date(dateLocal.setHours(dateLocal.getHours() - 5)).toISOString();
 
     const sourceData = {
-        infected: toNumber(infected),
-        tested: toNumber(tested),
-        recovered: toNumber(recovered),
-        deceased: toNumber(deceased)
+        infected: Number(infected),
+        tested: Number(tested),
+        recovered: Number(recovered),
+        deceased: Number(deceased),
+        critical: Number(critical)
     };
+
+    console.log(sourceData);
 
     const output = {
         ...sourceData,
         sourceUrl,
-        lastUpdatedAtSource: new Date(dateLocal).toISOString(),
+        // lastUpdatedAtSource: new Date(dateLocal).toISOString(),
         lastUpdatedAtApify: new Date(Date.now()).toISOString(),
         readMe: 'https://apify.com/cyberfly/covid-pk'
     };
