e983568cf7de9717899b8245be8c04df1b407c77
roblan <roberto.lange@gmail.com>
2020-11-17 17:29:34 +0100

    fix poland crawler

--- poland/src/main.js
+++ poland/src/main.js
@@ -21,8 +21,13 @@ Apify.main(async () => {
         handlePageFunction: async ({ $ }) => {
             log.info('Page loaded.');
             const now = new Date();
-            const rawData = JSON.parse(latinize($('pre#registerData').text().trim()));
-            const countryData = JSON.parse(rawData.parsedData);
+            try {
+                const rawData = JSON.parse(($('pre#registerData').text().trim());
+                const countryData = JSON.parse(rawData.parsedData);
+            } catch (e) {
+                check = true;
+                return;
+            }
             log.info(`${countryData.length} of regions loaded.`);
             const infectedByRegion = [];
             let infectedCountTotal = 0;
@@ -31,17 +36,17 @@ Apify.main(async () => {
                 check = true;
             }
             for (const region of countryData) {
-                const regionName = region.Wojewodztwo;
+                const regionName = region.Województwo;
                 const city = region['Powiat/Miasto'];
                 const infectedCount = region.Liczba ? parseInt(region.Liczba.replace(/ /g, '')) : 0;
-                const deceasedCount = region['Liczba zgonow'] ? parseInt(region['Liczba zgonow'].replace(/ /g, '')) : 0;
+                const deceasedCount = region['Liczba zgonów'] ? parseInt(region['Liczba zgonów'].replace(/ /g, '')) : 0;
 
-                if (regionName === 'Cala Polska') {
+                if (regionName === 'Cała Polska') {
                     infectedCountTotal = infectedCount;
                     deceasedCountTotal = deceasedCount;
                 } else {
                     infectedByRegion.push({
-                        region: regionName,
+                        region: latinize(regionName),
                         city,
                         infectedCount,
                         deceasedCount,
