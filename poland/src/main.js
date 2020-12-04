5681314248e1760e406288123027fede5bc760eb
Zuzana Pelechová <zpelechova@gmail.com>
2020-12-04 12:03:02 +0100

    poland changed overall numbers for total not daily

--- poland/src/main.js
+++ poland/src/main.js
@@ -1,32 +1,26 @@
 const Apify = require('apify');
 const latinize = require('latinize');
-
 const { log } = Apify.utils;
 const sourceUrl = 'https://www.gov.pl/web/koronawirus/wykaz-zarazen-koronawirusem-sars-cov-2';
 const LATEST = 'LATEST';
 let check = false;
-
 Apify.main(async () => {
-    const email = 'zpelechova@gmail.com'
-    // const { email } = await Apify.getValue('INPUT');
+    const { email } = await Apify.getValue('INPUT');
     const requestQueue = await Apify.openRequestQueue();
     const kvStore = await Apify.openKeyValueStore('COVID-19-POLAND');
     const dataset = await Apify.openDataset('COVID-19-POLAND-HISTORY');
-
     await requestQueue.addRequest({ url: sourceUrl });
     const crawler = new Apify.CheerioCrawler({
         requestQueue,
-        useApifyProxy: false,
-        // apifyProxyGroups: ['CZECH_LUMINATI'],
+        useApifyProxy: true,
+        apifyProxyGroups: ['CZECH_LUMINATI'],
         handlePageTimeoutSecs: 60 * 2,
         handlePageFunction: async ({ $ }) => {
             log.info('Page loaded.');
             const now = new Date();
-            
-            const rawData = JSON.parse($('pre#registerData').text().trim());
+            const rawData = JSON.parse(latinize($('pre#registerData').text().trim()));
             const countryData = JSON.parse(rawData.parsedData);
-            
-            log.info(`${countryData.length} regions loaded.`);
+            log.info(`${countryData.length} of regions loaded.`);
             const infectedByRegion = [];
             let infectedCountTotal = 0;
             let deceasedCountTotal = 0;
@@ -34,63 +28,53 @@ Apify.main(async () => {
                 check = true;
             }
             for (const region of countryData) {
-                const regionName = region.Województwo;
-                //const city = region['Powiat/Miasto'];
+                const regionName = region.Wojewodztwo;
+                const city = region['Powiat/Miasto'];
                 const infectedCount = region.Liczba ? parseInt(region.Liczba.replace(/ /g, '')) : 0;
-                const deceasedCount = region['Wszystkie przypadki śmiertelne'] ? parseInt(region['Wszystkie przypadki śmiertelne'].replace(/ /g, '')) : 0;
-
-                if (regionName === 'Cały kraj') {
-                    infectedCountTotal = infectedCount;
-                    deceasedCountTotal = deceasedCount;
-                } else {
-                    infectedByRegion.push({
-                        region: latinize(regionName),
-                        // city,
+                const deceasedCount = region['Liczba zgonow'] ? parseInt(region['Liczba zgonow'].replace(/ /g, '')) : 0;
+                infectedByRegion.push({
+                        region: regionName,
+                        city,
                         infectedCount,
                         deceasedCount,
-                    });
-                }
+                });
             }
+
+            const infected = $('.global-stats dl dt:contains(Liczba zakażonych od początku pandemii:)').next().text();
+            const deceased = $('.global-stats dl dt:contains(Liczba przypadków śmiertelnych:)').next().text();
+
             const data = {
-                infected: infectedCountTotal,
-                deceased: deceasedCountTotal,
+                infected: parseInt(infected),
+                deceased: parseInt(deceased),
                 infectedByRegion,
                 sourceUrl,
                 lastUpdatedAtApify: new Date(Date.UTC(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(), now.getMinutes())).toISOString(),
                 readMe: 'https://apify.com/vaclavrut/covid-pl',
             };
 
-            console.log(data)
-
+            console.log(data);
             // Compare and save to history
             const latest = await kvStore.getValue(LATEST);
-            if (latest) {delete latest.lastUpdatedAtApify};
+            delete latest.lastUpdatedAtApify;
             const actual = Object.assign({}, data);
             delete actual.lastUpdatedAtApify;
-
             if (JSON.stringify(latest) !== JSON.stringify(actual)) {
                 log.info('Data did change :( storing new to dataset.');
                 await dataset.pushData(data);
             }
-
             await kvStore.setValue(LATEST, data);
             log.info('Data stored, finished.');
-
             // to have lovely public runs...
             await Apify.pushData(data);
         },
-
         // This function is called if the page processing failed more than maxRequestRetries+1 times.
         handleFailedRequestFunction: async ({ request }) => {
             console.log(`Request ${request.url} failed twice.`);
         },
     });
-
     // Run the crawler and wait for it to finish.
     await crawler.run();
-
     console.log('Crawler finished.');
-
     // if there are no region data, send email, because that means something is wrong
     const env = await Apify.getEnv();
     if (check) {
